ae279212f6834d23ca9ffe5320e678d41c1621db
Noé <75133369+noebsc@users.noreply.github.com>
2025-08-09 21:57:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -103,10 +103,10 @@ onAuthStateChanged(auth, user => {
         // Si admin, ajouter bouton Ajouter projet
         if(user.email === ADMIN_EMAIL){
             const addBtn = document.createElement("button");
-            accountInfo.innerHTML = `<strong>"Administrateur"</strong>`;
+            accountInfo.innerHTML = `<strong>Administrateur 🔧</strong>`;
             addBtn.id = "add-project-btn";
             addBtn.textContent = "Ajouter un projet";
-            addBtn.style.marginLeft = "10px";
+            addBtn.style.marginLeft = "0px";
             addBtn.addEventListener("click", () => openProjectPopup("ajouter"));
             accountActions.appendChild(addBtn);
         }
