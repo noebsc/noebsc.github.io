754e215881795739557ef120587960391cebb176
Noé <75133369+noebsc@users.noreply.github.com>
2025-08-09 20:14:26 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyApPQ_SmqH-ZfGxRXO0k2j371FxfFN-QnA",
     authDomain: "noehub-2e774.firebaseapp.com",
@@ -7,9 +11,6 @@ const firebaseConfig = {
     appId: "1:677903842246:web:a30c11f9a19f95b02a50c7",
 };
 
-// Initialisation Firebase (ancienne syntaxe)
-firebase.initializeApp(firebaseConfig);
-
-// Auth et base Firestore
-const auth = firebase.auth();
-const db = firebase.firestore();
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
